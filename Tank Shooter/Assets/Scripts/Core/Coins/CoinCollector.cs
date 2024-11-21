064f043cfc629d58cc886280bdeb0e7c4bfb06ae
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2024-11-21 20:00:38 +0700

    add coin collector logic

---
+++ Tank Shooter/Assets/Scripts/Core/Coins/CoinCollector.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class CoinCollector : NetworkBehaviour
+{
+    public NetworkVariable<int> TotalCoins = new NetworkVariable<int>();
+
+    private int coinValue;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.TryGetComponent<Coin>(out Coin coin))
+        {
+            return;
+        }
+
+        coinValue = coin.Collect();
+
+        if (!IsServer) { return; }
+
+        TotalCoins.Value += coinValue;
+    }
+}
