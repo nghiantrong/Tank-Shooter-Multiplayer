922b9df02f0b35dba7e24196a762f9ca3c2b122b
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2024-11-28 20:26:42 +0700

    create allocation with the relay service

--- Tank Shooter/Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Tank Shooter/Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -1,9 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HostGameManager
 {
-    
+    private Allocation allocation;
+
+    private string joinCode;
+
+    private const int MAX_CONNECTIONS = 20;
+
+    private const string GAME_SCENE_NAME = "Game";
+
+    public async Task StartHostAsync()
+    {
+        try
+        {
+            //give allocation with maxConnections
+            allocation = await Relay.Instance.CreateAllocationAsync(MAX_CONNECTIONS);
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+            return;
+        }
+
+        try
+        {
+            //if succeed, get the code for it
+            joinCode = await Relay.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            Debug.Log("JoinCode: " + joinCode);
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+            return;
+        }
+
+        UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+
+        RelayServerData relayServerData = new RelayServerData(allocation, "udp");
+        transport.SetRelayServerData(relayServerData);
+
+        NetworkManager.Singleton.StartHost();
+
+        NetworkManager.Singleton.SceneManager.LoadScene(GAME_SCENE_NAME, LoadSceneMode.Single);
+    }
 }
