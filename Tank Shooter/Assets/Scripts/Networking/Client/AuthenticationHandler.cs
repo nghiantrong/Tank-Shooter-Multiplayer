2e47d4e1238fa8dc3333a7d256c186dbdaea6f1a
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2024-11-28 10:00:56 +0700

    improve script authenticationHandler

--- Tank Shooter/Assets/Scripts/Networking/Client/AuthenticationHandler.cs
+++ Tank Shooter/Assets/Scripts/Networking/Client/AuthenticationHandler.cs
@@ -1,41 +1,86 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine;
 
 public static class AuthenticationHandler
 {
     public static AuthState AuthState { get; private set; } = AuthState.NotAuthenticated;
 
-    public static async Task<AuthState> DoAuth(int maxTries = 5)
+    public static async Task<AuthState> DoAuth(int maxRetries = 5)
     {
         if (AuthState == AuthState.Authenticated)
         {
             return AuthState;
         }
 
+        if (AuthState == AuthState.Authenticating)
+        {
+            Debug.LogWarning("Already authenticating!");
+            await Authenticating();
+            return AuthState;
+        }
+
+        await SignInAnonymouslyAsync(maxRetries);
+
+        return AuthState;
+    }
+
+    private static async Task<AuthState> Authenticating()
+    {
+        while(AuthState == AuthState.Authenticating || 
+            AuthState == AuthState.NotAuthenticated)
+        {
+            await Task.Delay(200);
+        }
+
+        return AuthState;
+    }
+
+    private static async Task SignInAnonymouslyAsync(int maxRetries)
+    {
         AuthState = AuthState.Authenticating;
 
-        int tries = 0;
-        while(AuthState == AuthState.Authenticating && tries < maxTries)
+        int reTries = 0;
+        while (AuthState == AuthState.Authenticating && reTries < maxRetries)
         {
-            //player does not have to do anything to be signed in
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
-
-            if (AuthenticationService.Instance.IsSignedIn &&
-                AuthenticationService.Instance.IsAuthorized)
+            try
             {
-                AuthState = AuthState.Authenticated;
-                break;
+                //player does not have to do anything to be signed in
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+                if (AuthenticationService.Instance.IsSignedIn &&
+                    AuthenticationService.Instance.IsAuthorized)
+                {
+                    AuthState = AuthState.Authenticated;
+                    break;
+                }
+            }
+            catch (AuthenticationException ex)
+            {
+                Debug.LogError(ex);
+                AuthState = AuthState.Error;
+            }
+            catch (RequestFailedException exception)
+            {
+                Debug.LogError(exception);
+                AuthState = AuthState.Error;
             }
 
-            tries++;
+            reTries++;
             //1000 miliseconds
             await Task.Delay(1000);
         }
 
-        return AuthState;
+        //get through all tries but cannot be authenticated
+        if (AuthState != AuthState.Authenticated)
+        {
+            Debug.LogWarning($"Player was not signed in successfully after {reTries} tries");
+            AuthState = AuthState.Timeout;
+        }
     }
 }
 
