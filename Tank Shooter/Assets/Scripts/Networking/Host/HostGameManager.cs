9be2fa4b92e05e3ad47008ed06261befedeff1bd
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2024-12-02 19:08:12 +0700

    create lobby in hostGameManager

--- Tank Shooter/Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Tank Shooter/Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
@@ -16,6 +18,8 @@ public class HostGameManager
 
     private string joinCode;
 
+    private string lobbyId;
+
     private const int MAX_CONNECTIONS = 20;
 
     private const string GAME_SCENE_NAME = "Game";
@@ -24,7 +28,7 @@ public class HostGameManager
     {
         try
         {
-            //give allocation with maxConnections
+            //create allocation with maxConnections
             allocation = await Relay.Instance.CreateAllocationAsync(MAX_CONNECTIONS);
         }
         catch (Exception e)
@@ -51,9 +55,48 @@ public class HostGameManager
         RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
         transport.SetRelayServerData(relayServerData);
 
+        //create lobby
+        try
+        {
+            CreateLobbyOptions lobbyOptions = new CreateLobbyOptions();
+            lobbyOptions.IsPrivate = false;
+            lobbyOptions.Data = new Dictionary<string, DataObject>()
+            {
+                {
+                    "JoinCode", new DataObject (
+                        //if they are members of the lobby, allowed to read
+                        visibility: DataObject.VisibilityOptions.Member,
+                        value: joinCode
+                    )
+                }
+            };
+            Lobby lobby = await Lobbies.Instance.CreateLobbyAsync(
+                "My Lobby", MAX_CONNECTIONS, lobbyOptions);
+
+            lobbyId = lobby.Id;
+
+            //the ugs requires to send the ping after a certain amount of time so that it keeps enlisting the lobby
+            HostSingleton.Instance.StartCoroutine(HeartbeatLobby(15));
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.LogError(e);
+            return;
+        }
+
         NetworkManager.Singleton.StartHost();
 
         //only server needs to handle change scene because server changes scene for the client
         NetworkManager.Singleton.SceneManager.LoadScene(GAME_SCENE_NAME, LoadSceneMode.Single);
+    }
+
+    private IEnumerator HeartbeatLobby(float waitTimeSeconds)
+    {
+        WaitForSecondsRealtime delay = new WaitForSecondsRealtime(waitTimeSeconds);
+        while(true)
+        {
+            Lobbies.Instance.SendHeartbeatPingAsync(lobbyId);
+            yield return delay;
+        }
     }
 }
