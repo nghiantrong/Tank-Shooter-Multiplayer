89801a931c002064defb12bb4ed74c04a71a161b
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2024-12-05 20:27:15 +0700

    add virtual cam to the player

---
+++ Tank Shooter/Assets/Scripts/Core/Player/TankPlayer.cs
@@ -0,0 +1,22 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class TankPlayer : NetworkBehaviour
+{
+    [Header("References")]
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+
+    [Header("Settings")]
+    [SerializeField] private int ownerPriority = 15;
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            virtualCamera.Priority = ownerPriority;
+        }
+    }
+}
