357626721e82c5ec850f3055de532b2332330157
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2024-12-06 19:15:44 +0700

    handle respawning when player health is 0

---
+++ Tank Shooter/Assets/Scripts/Core/Combat/RespawnHandler.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class RespawnHandler : NetworkBehaviour
+{
+    [SerializeField] private NetworkObject playerPrefab;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsServer) return;
+
+        //players already in the scene
+        TankPlayer[] players = FindObjectsOfType<TankPlayer>();
+        foreach (TankPlayer player in players)
+        {
+            HandlePlayerSpawned(player);
+        }
+
+        //players who join after
+        TankPlayer.OnPlayerSpawned += HandlePlayerSpawned;
+        TankPlayer.OnPlayerDeSpawned += HandlePlayerDeSpawned;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (!IsServer) return;
+
+        TankPlayer.OnPlayerSpawned -= HandlePlayerSpawned;
+        TankPlayer.OnPlayerDeSpawned -= HandlePlayerDeSpawned;
+    }
+
+    private void HandlePlayerSpawned(TankPlayer player)
+    {
+        player.Health.OnDie += (health) => HandlePlayerDie(player);
+    }
+
+    private void HandlePlayerDeSpawned(TankPlayer player)
+    {
+        player.Health.OnDie -= (health) => HandlePlayerDie(player);
+    }
+    private void HandlePlayerDie(TankPlayer player)
+    {
+        Destroy(player.gameObject);
+
+        StartCoroutine(RespawnPlayer(player.OwnerClientId));
+    }
+
+    private IEnumerator RespawnPlayer(ulong ownerClientId)
+    {
+        yield return null;
+
+        NetworkObject playerInstance = Instantiate(
+            playerPrefab, SpawnPoint.GetRandomSpawnPos(), Quaternion.identity);
+
+        playerInstance.SpawnAsPlayerObject(ownerClientId);
+    }
+}
--- Tank Shooter/Assets/Scripts/Core/Player/TankPlayer.cs
+++ Tank Shooter/Assets/Scripts/Core/Player/TankPlayer.cs
@@ -1,4 +1,5 @@
 using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
@@ -9,6 +10,7 @@ public class TankPlayer : NetworkBehaviour
 {
     [Header("References")]
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [field: SerializeField] public Health Health { get; private set; }
 
     [Header("Settings")]
     [SerializeField] private int ownerPriority = 15;
@@ -17,6 +19,9 @@ public class TankPlayer : NetworkBehaviour
 
     public bool isPlayer;
 
+    public static event Action<TankPlayer> OnPlayerSpawned;
+    public static event Action<TankPlayer> OnPlayerDeSpawned;
+        
     public override void OnNetworkSpawn()
     {
         if (IsServer)
@@ -25,12 +30,22 @@ public class TankPlayer : NetworkBehaviour
                 HostSingleton.Instance.GameManager.NetworkServer.GetUserDataByClientId(OwnerClientId);
             
             PlayerName.Value = userData.userName;
+
+            OnPlayerSpawned?.Invoke(this);
         }
 
         if (IsOwner)
         {
             virtualCamera.Priority = ownerPriority;
             isPlayer = true;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsServer)
+        {
+            OnPlayerDeSpawned?.Invoke(this);
         }
     }
 }
