5618c278d4ddf7a477a16a2829b74b6d27de6c2b
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2024-12-11 19:11:10 +0700

    add healing zone logic

--- Tank Shooter/Assets/Scripts/Core/Combat/HealingZone.cs
+++ Tank Shooter/Assets/Scripts/Core/Combat/HealingZone.cs
@@ -16,7 +16,34 @@ public class HealingZone : NetworkBehaviour
     [SerializeField] private int coinsPerTick = 10;
     [SerializeField] private int healthPerTick = 10;
 
+    private float remainingCooldown;
+    private float tickTimer;
+
     private List<TankPlayer> playersInZone = new List<TankPlayer>();
+
+    private NetworkVariable<int> HealPower = new NetworkVariable<int>();
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsClient)
+        {
+            HealPower.OnValueChanged += HandleHealPowerChanged;
+            HandleHealPowerChanged(0, HealPower.Value);
+        }
+
+        if (IsServer)
+        {
+            HealPower.Value = maxHealPower;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsClient)
+        {
+            HealPower.OnValueChanged -= HandleHealPowerChanged;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -27,8 +54,6 @@ public class HealingZone : NetworkBehaviour
             return;
         }
         playersInZone.Add(player);
-
-        Debug.Log($"Entered: {player.PlayerName.Value}");
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -40,9 +65,60 @@ public class HealingZone : NetworkBehaviour
             return;
         }
         playersInZone.Remove(player);
-
-        Debug.Log($"Left: {player.PlayerName.Value}");
-
     }
 
+    private void Update()
+    {
+        if (!IsServer) { return; }
+
+        if (remainingCooldown > 0f)
+        {
+            remainingCooldown -= Time.deltaTime;
+
+            if (remainingCooldown < 0f)
+            {
+                HealPower.Value = maxHealPower;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        tickTimer += Time.deltaTime;
+        if (tickTimer >= 1 / healTickRate)
+        {
+            foreach (TankPlayer player in playersInZone)
+            {
+                if (HealPower.Value == 0) { break; }
+
+                if (player.Health.CurrentHealth.Value == player.Health.MaxHealth)
+                {
+                    continue;
+                }
+
+                if (player.CoinCollector.TotalCoins.Value < coinsPerTick)
+                {
+                    continue;
+                }
+
+                player.CoinCollector.SpendCoins(coinsPerTick);
+                player.Health.RestoreHealth(healthPerTick);
+
+                HealPower.Value -= 1;
+
+                if (HealPower.Value == 0)
+                {
+                    remainingCooldown = healCooldown;
+                }
+            }
+
+            tickTimer = tickTimer % (1 / healTickRate);
+        }
+    }
+
+    private void HandleHealPowerChanged(int oldHealPower, int newHealPower)
+    {
+        healPowerBar.fillAmount = (float)newHealPower / maxHealPower;
+    }
 }
